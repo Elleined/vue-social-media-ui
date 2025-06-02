2e5b169f2b19b7c564c6a5b8c788534612e315cd
elleined <demadegu@gmail.com>
2025-06-02 15:14:54 +0800

    Added primevue components

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 import ToastService from 'primevue/toastservice';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
-import {Button} from "primevue";
+import {
+    Avatar,
+    AvatarGroup,
+    Button,
+    Card,
+    ColorPicker,
+    DatePicker,
+    Dialog,
+    IftaLabel,
+    InputGroup,
+    InputGroupAddon,
+    InputText,
+    Message,
+    Password,
+    Textarea,
+    Toast
+} from "primevue";
 
 const app = createApp(App)
 
@@ -22,6 +38,21 @@ app.use(createPinia())
 app.use(router)
 app.use(ToastService)
 
+app.component("Card", Card)
 app.component("Button", Button)
+app.component("Dialog", Dialog)
+app.component("AvatarGroup", AvatarGroup)
+app.component("Avatar", Avatar)
+app.component("Message", Message)
+app.component("InputGroup", InputGroup)
+app.component("InputGroupAddon", InputGroupAddon)
+app.component("InputText", InputText)
+app.component("DatePicker", DatePicker)
+app.component("ColorPicker", ColorPicker)
+app.component("IftaLabel", IftaLabel)
+app.component("InpuText", InputText)
+app.component("TextArea", Textarea)
+app.component("Toast", Toast)
+app.component("Password", Password)
 
 app.mount('#app')
