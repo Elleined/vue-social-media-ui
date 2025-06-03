0be974fc32ee28bdfcb40be0ce67b8a5a72abc23
elleined <demadegu@gmail.com>
2025-06-03 16:30:02 +0800

    Added axios interceptor

--- src/utilities/APIClient.ts
+++ src/utilities/APIClient.ts
@@ -1,7 +1,13 @@
 import axios, {type AxiosInstance} from "axios";
 import {useAccessTokenStore} from "@/stores/AccessTokenStore.ts";
+import {useRefreshTokenStore} from "@/stores/RefreshTokenStore.ts";
+import {useRouter} from "vue-router";
 
 export const BASE_URL: string = `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}`
+
+const router = useRouter()
+const accessTokenStore = useAccessTokenStore()
+const refreshTokenStore = useRefreshTokenStore()
 
 export function APIClient(): AxiosInstance {
     return axios.create({
@@ -13,9 +19,9 @@ export function APIClient(): AxiosInstance {
 }
 
 export function APIClientWithCredentials(): AxiosInstance {
-    const accessToken = useAccessTokenStore().getPrincipal()
+    const accessToken = accessTokenStore.getPrincipal()
 
-    return axios.create({
+    const client = axios.create({
         baseURL: BASE_URL,
         headers: {
             'Content-Type': 'application/json',
@@ -23,4 +29,25 @@ export function APIClientWithCredentials(): AxiosInstance {
         },
         withCredentials: true
     })
+
+    client.interceptors.response.use(response => response, async error => {
+        try {
+            if (error.response?.status !== 401) {
+                return
+            }
+
+            const {data} = await APIClient().post('/users/refresh-tokens', {
+                refresh_token: refreshTokenStore.getPrincipal()
+            })
+
+            accessTokenStore.setPrincipal(data.access_token)
+            refreshTokenStore.setPrincipal(data.refresh_token)
+        } catch (e) {
+            accessTokenStore.setPrincipal("")
+            refreshTokenStore.setPrincipal("")
+            await router.push('/login')
+        }
+    })
+
+    return client
 }
