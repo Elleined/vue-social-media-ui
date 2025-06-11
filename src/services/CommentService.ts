e18bdd58a88a3f41013cd3e410dff3437c036239
elleined <demadegu@gmail.com>
2025-06-11 15:08:55 +0800

    Added commentservice and add validations

---
+++ src/services/CommentService.ts
@@ -0,0 +1,115 @@
+import type Page from "@/models/paging/Page.ts";
+import type {CommentReaction} from "@/models/CommentReaction.ts";
+import {APIClientWithCredentials} from "@/utilities/APIClient.ts";
+
+export const commentService = {
+    async save(postId: number, content: string, attachment: string): Promise<number> {
+        if (!postId || postId < 0)
+            throw new Error("please provide post id")
+
+        if (!content)
+            throw new Error("please provide content")
+
+        const response = await APIClientWithCredentials().post(`/users/posts/${postId}/comments`, {
+            content: content,
+            attachment: attachment
+        })
+
+        return response.data
+    },
+
+    async getAll(postId: number,
+                 page: number,
+                 size: number,
+                 field: string,
+                 sortBy: 'asc' | 'desc',
+                 isDeleted: boolean): Promise<Page<CommentReaction>> {
+
+        if (!postId || postId < 0)
+            throw new Error("please provide post id")
+
+        if (!page || page < 0)
+            throw new Error("please provide page number")
+
+        if (!size || size < 0)
+            throw new Error("please provide page size")
+
+        if (!field)
+            throw new Error("please provide field")
+
+        if (!sortBy)
+            throw new Error("please provide sort by")
+
+        const response = await APIClientWithCredentials().get(`/users/posts/${postId}/comments`, {
+            params: {
+                page: page,
+                pageSize: size,
+                field: field,
+                sortBy: sortBy,
+                isDeleted: isDeleted
+            }
+        })
+
+        return response.data
+    },
+
+    async updateContent(postId: number, commentId: number, content: string): Promise<string> {
+        if (!postId || postId < 0)
+            throw new Error("please provide post id")
+
+        if (!commentId || commentId < 0)
+            throw new Error("please provide comment id")
+
+        if (!content)
+            throw new Error("please provide content")
+
+        const response = await APIClientWithCredentials().patch(`/users/posts/${postId}/comments/${commentId}/content`, null, {
+            params: {
+                content: content
+            }
+        })
+
+        return response.data
+    },
+
+    async updateAttachment(postId: number, commentId: number, attachment: string): Promise<string> {
+        if (!postId || postId < 0)
+            throw new Error("please provide post id")
+
+        if (!commentId || commentId < 0)
+            throw new Error("please provide comment id")
+
+        if (!attachment)
+            throw new Error("please provide attachment")
+
+        const response = await APIClientWithCredentials().patch(`/users/posts/${postId}/comments/${commentId}/attachment`, null, {
+            params: {
+                attachment: attachment
+            }
+        })
+
+        return response.data
+    },
+
+    async deleteById(postId: number, commentId: number): Promise<void> {
+        if (!postId || postId < 0)
+            throw new Error("please provide post id")
+
+        if (!commentId || commentId < 0)
+            throw new Error("please provide comment id")
+
+        const response = await APIClientWithCredentials().delete(`/users/posts/${postId}/comments/${commentId}`)
+        return response.data
+    },
+
+    async getById(postId: number, commentId: number): Promise<CommentReaction> {
+        if (!postId || postId < 0)
+            throw new Error("please provide post id")
+
+        if (!commentId || commentId < 0)
+            throw new Error("please provide comment id")
+
+        const response = await APIClientWithCredentials().delete(`/users/posts/${postId}/comments/${commentId}`)
+        return response.data
+    },
+}
--- src/services/EmojiService.ts
+++ src/services/EmojiService.ts
@@ -3,6 +3,9 @@ import {APIClientWithCredentials} from "@/utilities/APIClient.ts";
 
 export const emojiService = {
     async save(name: string): Promise<number> {
+        if (!name)
+            throw new Error("please provide name")
+
         const response = await APIClientWithCredentials().post("/emojis", null, {
             params: {
                 name: name,
@@ -18,6 +21,12 @@ export const emojiService = {
     },
 
     async updateName(id: number, name: string): Promise<string> {
+        if (!id || id < 0)
+            throw new Error("please provide id")
+
+        if (!name)
+            throw new Error("please provide name")
+
         const response = await APIClientWithCredentials().patch(`/emojis/${id}`, null, {
             params: {
                 name: name,
@@ -28,11 +37,17 @@ export const emojiService = {
     },
 
     async deleteById(id: number): Promise<void> {
+        if (!id || id < 0)
+            throw new Error("please provide id")
+
         const response = await APIClientWithCredentials().delete(`/emojis/${id}`)
         return response.data
     },
 
     async getById(id: number): Promise<Emoji> {
+        if (!id || id < 0)
+            throw new Error("please provide id")
+
         const response = await APIClientWithCredentials().get(`/emojis/${id}`)
         return response.data
     },
